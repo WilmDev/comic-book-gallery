012b6ab0a1f8a01669f272db841f90a6de3c3c12
Kobi <kobiljon.yusupov@gmail.com>
2020-09-07 16:13:19 -0400

    Updated Comic Book Detail view to be strongl;y typed

--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -9,7 +9,7 @@ namespace ComicBookGallery.Models
     {
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
-        public int IsssueNumber { get; set; }
+        public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
@@ -18,7 +18,7 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle + " #" + IsssueNumber;
+                return SeriesTitle + " #" + IssueNumber;
             }
         }
 
@@ -26,7 +26,7 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IsssueNumber + ".jpg";
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
             }
         }
     }
